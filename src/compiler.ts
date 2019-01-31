5229a58efed3ce276fe4249d259952195f7360cf
Maurice de Laat <info@mauricedelaat.nl>
2019-01-31 17:00:46 +0100

    Updated copyright statements.

--- src/compiler.ts
+++ src/compiler.ts
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) 2018 Yellicode
+ * Copyright (c) 2019 Yellicode
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 ﻿#!/usr/bin/env node
 /*
- * Copyright (c) 2018 Yellicode
+ * Copyright (c) 2019 Yellicode
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
